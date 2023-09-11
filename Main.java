b37f1a2b1726fd60fb073586ddf9339ffe332214
Bernardo Martinelli <bermartinelli@gmail.com>
2023-09-11 07:44:40 -0300

    se agregan metodos para carga de CSVs

--- Main.java
+++ Main.java
@@ -4,12 +4,16 @@ import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 
+import Entidades.*;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.SQLException;
 import Factory.DAOFactory;
 
 
+import static java.lang.Float.parseFloat;
+import static java.lang.Integer.parseInt;
 
 public class Main {
 
@@ -29,7 +33,39 @@ public class Main {
        daoProducto.createTable();
        daoFacturaProducto.createTable();
 
-       //CONTINUAR DESDE ACA CON LA CARGA DE LOS CSV!!!!
+       //CARGA DE CLIENTES
+        CSVParser parserCliente = CSVFormat.DEFAULT.withHeader().parse(new
+                FileReader("csv/clientes.csv"));
+        for(CSVRecord row: parserCliente) {
+            Cliente cliente = new Cliente(parseInt(row.get("idCliente")),row.get("nombre"),row.get("email"));
+            daoCliente.insert(cliente);
+        }
+
+        //CARGA DE FACTURAS
+        CSVParser parserFactura = CSVFormat.DEFAULT.withHeader().parse(new
+                FileReader("csv/facturas.csv"));
+        for(CSVRecord row: parserFactura) {
+            Factura factura = new Factura(parseInt(row.get("idFactura")), parseInt(row.get("idCliente")));
+            daoFactura.insert(factura);
+        }
+
+        //CARGA DE PRODUCTOS
+        CSVParser parserProducto = CSVFormat.DEFAULT.withHeader().parse(new
+                FileReader("csv/productos.csv"));
+        for(CSVRecord row: parserProducto) {
+            Producto producto = new Producto(parseInt(row.get("idProducto")), row.get("nombre"), parseFloat(row.get("valor")));
+            daoProducto.insert(producto);
+        }
+
+        //CARGA DE FACTURA PRODUCTOS
+        CSVParser parserFP = CSVFormat.DEFAULT.withHeader().parse(new
+                FileReader("csv/facturas-productos.csv"));
+        for(CSVRecord row: parserFP) {
+            Factura_Producto FP = new Factura_Producto(parseInt(row.get("idFactura")), parseInt(row.get("idProducto")), parseInt(row.get("cantidad")));
+            daoFacturaProducto.insert(FP);
+        }
+
+
 
     }
 }
